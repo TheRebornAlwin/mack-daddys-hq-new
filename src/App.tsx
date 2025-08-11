acba453d500583e421df8f4ea63244bfa4bcabdb
AlwinTheChen <ac3116735555@gmail.com>
2025-08-11 21:30:04 +0100

    Fix blank screen issue and republish

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,24 @@ import CoursesPage from './pages/CoursesPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/home" element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/order" element={<CheckoutPage />} />
-      <Route path="/special-offer" element={<UpsellPage />} />
-      <Route path="/exclusive-upgrade" element={<UpsellPage />} />
-      <Route path="/thank-you" element={<ThankYouPage />} />
-      <Route path="/success" element={<ThankYouPage />} />
-      <Route path="/upsell-success" element={<UpsellThankYouPage />} />
-      <Route path="/vip-welcome" element={<UpsellThankYouPage />} />
-      <Route path="/terms" element={<TermsPage />} />
-      <Route path="/privacy" element={<PrivacyPage />} />
-      <Route path="/contact" element={<ContactPage />} />
-      <Route path="/courses" element={<CoursesPage />} />
-    </Routes>
+    <div className="min-h-screen">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/order" element={<CheckoutPage />} />
+        <Route path="/special-offer" element={<UpsellPage />} />
+        <Route path="/exclusive-upgrade" element={<UpsellPage />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+        <Route path="/success" element={<ThankYouPage />} />
+        <Route path="/upsell-success" element={<UpsellThankYouPage />} />
+        <Route path="/vip-welcome" element={<UpsellThankYouPage />} />
+        <Route path="/terms" element={<TermsPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/courses" element={<CoursesPage />} />
+      </Routes>
+    </div>
   );
 }
 
